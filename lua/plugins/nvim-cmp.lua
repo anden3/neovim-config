8df0efe0b557d439454326f851ba83435c75289c
André Vennberg <andre.vennberg@gmail.com>
2024-01-09 14:39:37 +0100

    Navigate between snippet positions with tab.

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -13,6 +13,7 @@ local M = {
 
 M.config = function()
     local cmp = require("cmp")
+    local luasnip = require("luasnip")
 
     vim.opt.completeopt = { "menu", "menuone", "noselect", "preview" }
     vim.opt.shortmess = vim.opt.shortmess + "c"
@@ -21,8 +22,24 @@ M.config = function()
         ["<C-p>"] = cmp.mapping.select_prev_item(),
         ["<C-n>"] = cmp.mapping.select_next_item(),
         -- Add tab support
-        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-        ["<Tab>"] = cmp.mapping.select_next_item(),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, { "i" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i" }),
         ["<C-d>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
@@ -57,7 +74,7 @@ M.config = function()
 
     local snippet = {
         expand = function(args)
-            require("luasnip").lsp_expand(args.body)
+            luasnip.lsp_expand(args.body)
         end,
     }
 
